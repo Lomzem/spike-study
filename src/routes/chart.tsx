17c42ea35ce482dfc0aaeddd1807cebe4cfc6711
Lawjay Lee <lawjaylee1@icloud.com>
2026-03-01 22:29:27 -0800

    chore: begin StockInfoBar

--- src/routes/chart.tsx
+++ src/routes/chart.tsx
@@ -4,9 +4,16 @@ export const Route = createFileRoute('/chart')({
   component: RouteComponent,
 })
 
+function StockInfoBar() {
+  return (
+    <section className="flex gap-2 items-center py-1 px-3 border-b shrink-0 bg-bg border-border"></section>
+  )
+}
+
 function RouteComponent() {
   return (
     <>
+      <StockInfoBar />
       <Outlet />
     </>
   )
