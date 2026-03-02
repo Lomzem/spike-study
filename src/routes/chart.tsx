2fabcaa0dde117ed3b605be5786aa00a60c26a73
Lawjay Lee <lawjaylee1@icloud.com>
2026-03-01 22:31:05 -0800

    feat: symbol input, install shadcn ui

--- src/routes/chart.tsx
+++ src/routes/chart.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router'
+import { Input } from '~/components/ui/input'
 
 export const Route = createFileRoute('/chart')({
   component: RouteComponent,
@@ -6,7 +7,15 @@ export const Route = createFileRoute('/chart')({
 
 function StockInfoBar() {
   return (
-    <section className="flex gap-2 items-center py-1 px-3 border-b shrink-0 bg-bg border-border"></section>
+    <section className="flex gap-2 items-center py-1 px-3 border-b shrink-0 bg-bg border-border">
+      <label className="font-mono tracking-wider uppercase">Symbol</label>
+      <Input
+        type="text"
+        className="py-0.5 px-2 w-20 font-mono text-xs font-bold rounded-md outline-none border-border bg-surface text-md text-primary"
+        defaultValue="AAPL"
+        spellCheck={false}
+      />
+    </section>
   )
 }
 
