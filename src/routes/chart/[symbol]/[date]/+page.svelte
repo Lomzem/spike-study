aa04a76e00c70cc20026d626b787b043e3462c9a
Lawjay Lee <lawjaylee1@icloud.com>
2026-04-07 23:51:15 -0700

    feat: volume

--- src/routes/chart/[symbol]/[date]/+page.svelte
+++ src/routes/chart/[symbol]/[date]/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-import { CandlestickSeries, createChart, type UTCTimestamp } from 'lightweight-charts';
-import { onMount } from 'svelte';
+import {
+  CandlestickSeries,
+  createChart,
+  HistogramSeries,
+  type UTCTimestamp,
+} from 'lightweight-charts';
+import type { Attachment } from 'svelte/attachments';
 
 let { data } = $props();
 
-let chartElement: HTMLElement | undefined = $state(undefined);
-
-onMount(() => {
-  if (!chartElement) return;
-
+const createChartAttachment: Attachment<HTMLElement> = (chartElement) => {
   const chart = createChart(chartElement, {
     timeScale: {
       timeVisible: true,
@@ -17,13 +18,17 @@ onMount(() => {
     height: chartElement.clientHeight,
   });
 
-  const candlestickSeries = chart.addSeries(CandlestickSeries, {
-    upColor: '#26a69a',
-    downColor: '#ef5350',
-    borderVisible: false,
-    wickUpColor: '#26a69a',
-    wickDownColor: '#ef5350',
-  });
+  const candlestickSeries = chart.addSeries(
+    CandlestickSeries,
+    {
+      upColor: '#26a69a',
+      downColor: '#ef5350',
+      borderVisible: false,
+      wickUpColor: '#26a69a',
+      wickDownColor: '#ef5350',
+    },
+    0,
+  );
   candlestickSeries.setData(
     data.intradayData.map((row) => ({
       time: Math.floor(row.time / 1000) as UTCTimestamp,
@@ -34,8 +39,19 @@ onMount(() => {
     })),
   );
 
+  const volumeSeries = chart.addSeries(HistogramSeries, { priceFormat: { type: 'volume' } }, 1);
+  volumeSeries.setData(
+    data.intradayData.map((row) => ({
+      time: Math.floor(row.time / 1000) as UTCTimestamp,
+      value: row.volume,
+      color: row.close > row.open ? '#26a69a' : '#ef5350',
+    })),
+  );
+
+  const candlesPane = chart.panes()[0];
+  candlesPane.setHeight((chartElement.clientHeight * 3) / 4);
+
   const resizeObserver = new ResizeObserver(() => {
-    if (!chartElement) return;
     const width = chartElement.clientWidth;
     const height = chartElement.clientHeight;
     if (width <= 0 || height <= 0) return;
@@ -47,7 +63,7 @@ onMount(() => {
     resizeObserver.disconnect();
     chart.remove();
   };
-});
+};
 </script>
 
 {#if data.intradayData.length === 0}
@@ -55,5 +71,5 @@ onMount(() => {
     <span class="text-center text-xl">No data for {data.symbol} on {data.date}</span>
   </p>
 {:else}
-  <main bind:this={chartElement} class="h-dvh w-dvw"></main>
+  <main {@attach createChartAttachment} class="h-dvh w-dvw"></main>
 {/if}
